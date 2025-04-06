50ffdd1618c282615f3b0931713807f4920960b1
qarzhaubayev <106156036+qarzhaubayev@users.noreply.github.com>
2025-04-06 16:09:55 +0000

    Main file created

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum class State {
+    IDLE,
+    WAITING_FOR_OBJECT,
+    DISPENSING
+};
+
+int main() {
+
+    
+    return 0;
+}
